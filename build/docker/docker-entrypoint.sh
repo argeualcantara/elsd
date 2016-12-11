6ba4e4a5982e4250a6cd3e0001e90a2d6f2c1dd6
Leonardo Eloy <eloy@hp.com>
2016-12-11 15:15:23 -0700

    Adding vendored depencendies

---
+++ build/docker/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# (C) Copyright 2016 HP Development Company, L.P.
+# Confidential computer software. Valid license from HP required for possession, use or copying.
+# Consistent with FAR 12.211 and 12.212, Commercial Computer Software,
+# Computer Software Documentation, and Technical Data for Commercial Items are licensed
+# to the U.S. Government under vendor's standard commercial license.
+#
+echo "=> Loading container environment variables..."
+declare -x ELS_ADDRESS=${ELS_ADDRESS}
+declare -x ELS_PORT=${ELS_PORT}
+declare -x ELS_DEBUG=${ELS_DEBUG}
+
+echo "=> Starting ELS..."
+/els
